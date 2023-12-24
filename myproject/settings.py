a76dd93211779ed3d1cbf109c3718d16ba8a1f66
KhanhS <20521456@gm.uit.edu.vn>
2023-12-25 00:33:22 +0700

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'Songhyekyo000@',
+        'PASSWORD': 'XXX',
         'HOST': 'localhost',
         'PORT': '5432'
     }
